2f2ef011cadb81620f76020fab74f453f6d434fb
Kristopher Johnson <kris@kristopherjohnson.net>
2019-03-27 07:04:11 -0400

    Factor out simulate_attacks() and report_results() from main()

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,6 @@
 extern crate risk_odds;
 
 use risk_odds::{percentage, Attack, Die, Score};
-
 use std::env;
 
 fn main() {
@@ -15,13 +14,22 @@ fn main() {
         return;
     }
 
+    let (wins, losses, ties) = simulate_attacks(attack_count);
+
+    report_results(wins, losses, ties);
+}
+
+/// Simulate the specified number of attacks.
+///
+/// Returns a tuple `(wins, losses, ties)`.
+fn simulate_attacks(count: i32) -> (i32, i32, i32) {
     let mut die = Die::new();
 
     let mut wins = 0;
     let mut losses = 0;
     let mut ties = 0;
 
-    for _ in 1..=attack_count {
+    for _ in 0..count {
         match Attack::with_die(&mut die).attacker_score() {
             Score::Win => wins += 1,
             Score::Loss => losses += 1,
@@ -29,17 +37,22 @@ fn main() {
         }
     }
 
+    (wins, losses, ties)
+}
+
+/// Print the results of a series of attacks.
+fn report_results(wins: i32, losses: i32, ties: i32) {
     let total = wins + losses + ties;
 
     println!(
         "Wins   {:12} {:6.2}%\nLosses {:12} {:6.2}%\nTies   {:12} {:6.2}%\nTotal  {:12} {:6.2}%",
         wins,
-        percentage(wins, attack_count),
+        percentage(wins, total),
         losses,
-        percentage(losses, attack_count),
+        percentage(losses, total),
         ties,
-        percentage(ties, attack_count),
+        percentage(ties, total),
         total,
-        percentage(total, attack_count)
+        percentage(total, total)
     );
 }
